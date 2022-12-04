bad84a4050c4583af8389adc1a99a51881141855
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-04 03:30:17 +0100

    Bump minor version

--- vsiew/_metadata.py
+++ vsiew/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from Irrational Encoding Wizardry"""
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 __author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
 __maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
