51bfa9caa86ab72ceba88746b65b72aab9489557
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-01-02 23:47:13 +0100

    Allow every package to be imported through vsiew

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ Install `vsiew` with the following command:
 ```sh
 pip install vsiew
 ```
+
+## How to use
+
+Once you have installed this package, you can import all the IEW packages directly from vsiew:
+
+```py
+from vsiew import descale, BM3DCuda, depth
+```
+
+or populate your entire IDE with every IEW function using a star import:
+
+```py
+from vsiew import *
+```
--- vsiew/__init__.py
+++ vsiew/__init__.py
@@ -1,1 +1,18 @@
+# flake8: noqa
+
+from lvsfunc import *
+from vsaa import *
+from vsdeband import *
+from vsdehalo import *
+from vsdeinterlace import *
+from vsdenoise import *
+from vsdfft import *
+from vsexprtools import *
+from vskernels import *
+from vsmasktools import *
+from vsparsedvd import *
+from vsrgtools import *
+from vsscale import *
+from vstools import *
+
 from .init import update  # noqa: F401
