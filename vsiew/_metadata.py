3263c841771c18bab4939e015f0c9389e965593c
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-30 20:08:47 +0100

    Bump packages

--- vsiew/_metadata.py
+++ vsiew/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from Irrational Encoding Wizardry"""
 
-__version__ = '1.4.9'
+__version__ = '1.5.0'
 
 __author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
 __maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
