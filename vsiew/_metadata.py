39293ff04c626cdf28e18335d47aaaac09931a40
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-30 11:10:24 +0100

    Bump vskernels

--- vsiew/_metadata.py
+++ vsiew/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from Irrational Encoding Wizardry"""
 
-__version__ = '1.4.8'
+__version__ = '1.4.9'
 
 __author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
 __maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
