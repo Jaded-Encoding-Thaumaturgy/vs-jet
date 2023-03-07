3154efa96982b2ed313e710a57e5e5205ee5eff9
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-03-07 20:49:09 +0100

    Stable vstools

--- vsiew/_metadata.py
+++ vsiew/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from Irrational Encoding Wizardry"""
 
-__version__ = '1.6.3'
+__version__ = '1.6.4'
 
 __author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
 __maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
