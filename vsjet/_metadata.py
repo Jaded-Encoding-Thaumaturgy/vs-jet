6f77dd14a8d8927a41d0d6497392451abae45e68
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2025-01-19 01:24:53 -0500

    Bump minor version

--- vsjet/_metadata.py
+++ vsjet/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from JET"""
 
-__version__ = "3.4.2"
+__version__ = "3.4.3"
 
 __author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
