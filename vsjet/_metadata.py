9243b2bbdd4351b58a6f8938157e6d769bb8d810
arch1t3cht <arch1t3cht@gmail.com>
2025-02-13 15:21:40 +0100

    Final version bump to show deprecation warning

--- vsjet/_metadata.py
+++ vsjet/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth packages from JET"""
 
-__version__ = "3.5.2"
+__version__ = "3.5.3"
 
 __author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
